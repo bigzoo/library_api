bb18cb90f916c1b5825688e3f20c25397f8fe16d
Chris Nyaga <nyagachris75@gmail.com>
2017-08-24 11:44:27 +0300

    New rescue method for wrong routes added.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
   include Response
+  rescue_from ActionController::RoutingError do |exception|
+    json_response({ error: exception.message }, :bad_request)
+  end
   rescue_from ActiveRecord::RecordNotFound do |exception|
     json_response({ error: exception.message }, :not_found)
   end
