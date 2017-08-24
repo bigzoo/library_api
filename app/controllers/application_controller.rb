bcbb8aa376897ebcb524e29fbb5ea843985a1187
Chris Nyaga <nyagachris75@gmail.com>
2017-08-24 10:33:23 +0300

    Rescues for active record errors added to application controller and Respense module included for parsing json.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  include Response
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    json_response({ error: exception.message }, :not_found)
+  end
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    json_response({ error: exception.message }, :unprocessable_entity)
+  end
 end
