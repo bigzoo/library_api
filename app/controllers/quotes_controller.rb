999ef1901fae7f9295b8cc8bdf84618476ca9b66
Chris Nyaga <nyagachris75@gmail.com>
2017-08-24 11:45:29 +0300

    Methods added to the quotes controller for different methods.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,11 +1,43 @@
 class QuotesController < ApplicationController
   def index
-  @quotes = {"quotation": "The secret of getting ahead is getting started."}
-  json_response(@quotes)
-end
+    if params[:name]
+      author = params[:name]
+      @quotes = Quote.search(author)
+    else
+      @quotes = Quote.all
+    end
+    json_response(@quotes)
+  end
 
-private
-def json_response(object)
-  render json: object, status: 200
-end
+  def show
+    @quote = Quote.find(params[:id])
+    json_response(@quote)
+  end
+
+  def create
+    @quote = Quote.create!(quote_params)
+    json_response(@quote, :created)
+  end
+
+  def update
+    @quote = Quote.find(params[:id])
+    @quote.update!(quote_params)
+  end
+
+  def destroy
+    @quote = Quote.find(params[:id])
+    @quote.destroy
+    render status: 200, json: {
+       message: "Your quote has successfully been delete."
+    }
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def quote_params
+    params.permit(:author, :content)
+  end
 end
